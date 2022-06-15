c7455d5b3a3a6c8612ad8aba82bcc20afb6b196c
KDY <rlaeodud9983@naver.com>
2022-06-15 20:27:46 +0900

    [CREATE] ADD initGoogle gso, checkGsoLogin 함수 추가

--- app/src/main/java/org/gsm/olio/view/fragment/login/LoginFragment.kt
+++ app/src/main/java/org/gsm/olio/view/fragment/login/LoginFragment.kt
@@ -1,60 +1,60 @@
 package org.gsm.olio.view.fragment.login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.navigation.fragment.findNavController
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import org.gsm.olio.R
+import org.gsm.olio.databinding.FragmentLoginBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val binding by lazy { FragmentLoginBinding.inflate(layoutInflater) }
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var requestActivity : ActivityResultLauncher<Intent>
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View? = with(binding) {
+        lifecycleOwner = this@LoginFragment
+        fragment = this@LoginFragment
+        initGso()
+
+
+        return root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onStart() {
+        checkGso()
+        super.onStart()
     }
+
+    private fun checkGso(){
+        val account = GoogleSignIn.getLastSignedInAccount(requireContext())
+        account?.run { findNavController().navigate(R.id.action_loginFragment_to_mainFragment) }
+    }
+
+    private fun initGso(){
+      val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .requestId()
+            .build()
+
+       googleSignInClient = GoogleSignIn.getClient(requireContext(),gso)
+
+    }
+
+
+
+
 }
