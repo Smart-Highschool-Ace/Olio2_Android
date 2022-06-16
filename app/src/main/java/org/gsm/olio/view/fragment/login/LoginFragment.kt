ed16fca3c27773ed65fef1d7ba0806e2ad68622f
KDY <rlaeodud9983@naver.com>
2022-06-16 23:00:45 +0900

    [UPDATE] Login 함수 추가

--- app/src/main/java/org/gsm/olio/view/fragment/login/LoginFragment.kt
+++ app/src/main/java/org/gsm/olio/view/fragment/login/LoginFragment.kt
@@ -1,12 +1,15 @@
 package org.gsm.olio.view.fragment.login
 
+import android.app.appsearch.AppSearchResult.RESULT_OK
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.fragment.findNavController
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -15,7 +18,7 @@ import org.gsm.olio.R
 import org.gsm.olio.databinding.FragmentLoginBinding
 
 
-class LoginFragment : Fragment() {
+open class LoginFragment : Fragment() {
 
     private val binding by lazy { FragmentLoginBinding.inflate(layoutInflater) }
     private lateinit var googleSignInClient: GoogleSignInClient
@@ -29,6 +32,7 @@ class LoginFragment : Fragment() {
         lifecycleOwner = this@LoginFragment
         fragment = this@LoginFragment
         initGso()
+        resultActivity()
 
 
         return root
@@ -46,14 +50,35 @@ class LoginFragment : Fragment() {
 
     private fun initGso(){
       val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.server_client_id))
             .requestEmail()
-            .requestId()
             .build()
 
        googleSignInClient = GoogleSignIn.getClient(requireContext(),gso)
 
     }
 
+    fun signIn(){
+        var signInIntent: Intent = googleSignInClient.signInIntent
+        requestActivity.launch(signInIntent)
+    }
+
+    private fun resultActivity(){
+        requestActivity =registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+            if (it.resultCode == RESULT_OK){
+                try {
+                    val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+                    val account = task.result!!
+                    Log.d(TAG, "resultActivity: 성공")
+                    findNavController().navigate(R.id.action_loginFragment_to_mainFragment)
+                }catch (e : ApiException){
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
 
 
 
