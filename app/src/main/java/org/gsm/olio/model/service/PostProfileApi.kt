4bca09d9ae1c5a5092987700f26052edc0da116d
KDY <rlaeodud9983@naver.com>
2022-09-06 23:46:44 +0900

    [FEAT] not encoding

--- app/src/main/java/org/gsm/olio/model/service/PostProfileApi.kt
+++ app/src/main/java/org/gsm/olio/model/service/PostProfileApi.kt
@@ -2,17 +2,15 @@ package org.gsm.olio.model.service
 
 import okhttp3.MultipartBody
 import okhttp3.Response
-import retrofit2.http.Multipart
-import retrofit2.http.PUT
-import retrofit2.http.Part
-import retrofit2.http.Path
+import retrofit2.http.*
 
 interface PostProfileApi {
+    @Headers("Accept-Encoding: identity;")
     @Multipart
     @PUT("{uploadURL}")
     suspend fun postProfile(
-        @Path("uploadURL") url : String,
-        @Part image: MultipartBody.Part?
+        @Path("uploadURL", encoded = true) url : String,
+        @Part image: MultipartBody.Part
     ):retrofit2.Response<String>
 
 }
