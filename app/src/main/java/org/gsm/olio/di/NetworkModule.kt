b905c994418e8c1b6d8fdff45c074b4218b1643c
KDY <rlaeodud9983@naver.com>
2022-09-26 18:17:47 +0900

    [FEAT] Multipart File Response로 발생하는 이전 Log Message 지워지는 문제 해결

--- app/src/main/java/org/gsm/olio/di/NetworkModule.kt
+++ app/src/main/java/org/gsm/olio/di/NetworkModule.kt
@@ -8,18 +8,19 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Request
 import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import org.gsm.olio.BuildConfig
-import org.gsm.olio.MyApplication
 import org.gsm.olio.model.service.PostProfileApi
 import org.gsm.olio.model.service.UploadImageApi
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
-import javax.inject.Singleton
+import java.io.IOException
 import java.util.concurrent.TimeUnit
 import javax.inject.Qualifier
+import javax.inject.Singleton
+
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -36,11 +37,11 @@ object NetworkModule {
 
     @Provides
     @Singleton
-    fun apolloClient(okHttpClient: OkHttpClient): ApolloClient {
+    fun apolloClient(): ApolloClient {
 
         return ApolloClient.Builder()
-            .okHttpClient(okHttpClient)
             .serverUrl(BuildConfig.BASE_URL)
+            .okHttpClient(okHttpClient())
             .build()
 
     }
@@ -48,26 +49,27 @@ object NetworkModule {
     @Provides
     @Singleton
     @type1
-    fun retrofit(okHttpClient: OkHttpClient):Retrofit{
+    fun retrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BuildConfig.IMAGE_URL)
-            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient())
             .build()
     }
 
     @Provides
     @Singleton
     @type1
-    fun provideImgUpload(@type1 retrofit: Retrofit) : UploadImageApi =retrofit.create(UploadImageApi::class.java)
+    fun provideImgUpload(@type1 retrofit: Retrofit): UploadImageApi =
+        retrofit.create(UploadImageApi::class.java)
 
     @Provides
     @Singleton
     @type2
-    fun retrofit2(okHttpClient: OkHttpClient):Retrofit{
+    fun retrofit2(): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BuildConfig.POST_PROFILE_URL)
-            .client(okHttpClient)
+            .client(okHttpClient())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
@@ -75,40 +77,58 @@ object NetworkModule {
     @Provides
     @Singleton
     @type2
-    fun postProfile(@type2 retrofit: Retrofit) : PostProfileApi = retrofit.create(PostProfileApi::class.java)
+    fun postProfile(@type2 retrofit: Retrofit): PostProfileApi =
+        retrofit.create(PostProfileApi::class.java)
 
     @Provides
     @Singleton
-    fun provideHttpClient(): OkHttpClient {
-        return OkHttpClient.Builder()
+    fun okHttpClient(): OkHttpClient {
+        val builder = OkHttpClient.Builder()
+        val logger = httpLogger(builder)
+        return builder
             //서버로부터의 응답까지의 시간이 읽기 시간 초과보다 크면 요청 실패로 판단한다.
             .readTimeout(10, TimeUnit.SECONDS)
             //서버로 요청을 시작한 후 15초가 지날 때 까지 데이터가 안오면 에러로 판단한다.
             .connectTimeout(10, TimeUnit.SECONDS)
             // 얼마나 빨리 서버로 데이터를 받을 수 있는지 판단한다.
             .writeTimeout(15, TimeUnit.SECONDS)
-            .// 이 클라이언트를 통해 오고 가는 네트워크 요청/응답을 로그로 표시하도록 합니다.
-            addInterceptor(getLoggingInterceptor())
-            .addInterceptor(AuthorizationInterceptor())
+            //Header 추가
+            .addInterceptor(MyInterceptor())
+            //서버로 부터 받아온 데이터 log 찍기
+            .addInterceptor(logger)
             .build()
     }
 
-    // 서버로 부터 받아온 데이터 log 찍기
-    private fun getLoggingInterceptor(): HttpLoggingInterceptor =
-        HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+    private fun httpLogger(builder: OkHttpClient.Builder) : HttpLoggingInterceptor {
+        val logger = HttpLoggingInterceptor()
+        //Multipart response Log 메세지 제외
+        builder.addInterceptor {
+            val original : Request = it.request()
+            val request : Request = original.newBuilder().build()
+            val hasMultipart = request.headers.names().contains("multipart")
+            //None으로 할 시 Request요청도 안보이므로 HEADERS로 바꿔 Request, Response 메세지만 본다.
+            logger.level = (if(hasMultipart) HttpLoggingInterceptor.Level.HEADERS else HttpLoggingInterceptor.Level.BODY)
+            return@addInterceptor it.proceed(request)
+        }.build()
 
+        return logger
+    }
 
 }
 
+class MyInterceptor : Interceptor {
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response = with(chain) {
 
-private class AuthorizationInterceptor() : Interceptor {
+        val request: Request = chain.request()
+        var stringurl = request.url.toString()
+        stringurl = stringurl.replace("%3F", "?")
 
-    override fun intercept(chain: Interceptor.Chain): Response {
-
-        val request = chain.request().newBuilder()
-            .addHeader("Authorization", MyApplication.prefs.token ?: "")
+        val newRequest = request().newBuilder()
+            .url(stringurl)
             .build()
 
-        return chain.proceed(request)
+        return proceed(newRequest)
     }
+
 }
